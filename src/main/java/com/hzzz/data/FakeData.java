fcbc8d257f374ffddd93cf231d091ed7033772fc
hzzz <344144460@qq.com>
2017-04-28 14:20:38 +0800

    3

--- src/main/java/com/hzzz/data/FakeData.java
+++ src/main/java/com/hzzz/data/FakeData.java
@@ -14,5 +14,5 @@ public class FakeData {
             new Commodity("1", "Apple MacBook Air MJVE2LL/A 13-inch Laptop", Category.MACBOOKS, "1.49kg"),
             new Commodity("2", "Apple MacBook Pro", Category.MACBOOKS, "3.3kg"),
             new Commodity("3", "lenovo Air", Category.LENOVO, "10.0mm-15.0mm"),
-            new Commodity("5", "DELL 7000 R1605S", Category.DELL, null));
+            new Commodity("4", "DELL 7000 R1605S", Category.DELL, null));
 }
